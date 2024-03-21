bed0b4b480fbd2a3e94b7469c38378c13d127020
Jovan <knife747@gmail.com>
2024-03-21 01:07:00 +0100

    Changes in student model

--- server/src/models/student.model.ts
+++ server/src/models/student.model.ts
@@ -1,34 +1,31 @@
-import { UniDocument } from '../models/university.model';
 import { Document, Schema, model, Types } from 'mongoose';
 import { UserDocument } from './user.model';
 
 export interface StudentDocument extends Document {
-    userId: UserDocument;
-    studentId: string;
-    modelNum?: string;
-    subjects: string[];
-    completedSubjects: string[];
-    department: string;
-    degree: string;
-    status: string;
-    dateOfEnrollment: string;
-    currentSemester: string;
-    grades: string[];
-    universities: UniDocument[]
+	userId: UserDocument;
+	studentId: string;
+	modelNum?: string;
+	subjects: string[];
+	completedSubjects: string[];
+	department: string;
+	degree: string;
+	status: string;
+	dateOfEnrollment: string;
+	currentSemester: string;
+	grades: string[];
 }
 
 const StudentSchema = new Schema({
-    studentId: { type: String, required: true },
-    modelNum: { type: String, required: true },
-    subjects: [{ type: String, ref: 'Subject', index: true  }], // Obj.Id
-    completedSubjects: [{ type: Schema.ObjectId, ref: 'Subject', index: true }], // Obj.Id
-    department: { type: Schema.ObjectId, ref: 'Department', required: true }, // Obj.Id
-    degree: { type: String, enum: ['OAS', 'MAS', 'DAS'] },
-    status: { type: String, enum: ['Budžet', 'Samofinansiranje', 'Done'], required: true },
-    dateOfEnrollment: { type: String, required: true },
-    currentSemester: { type: String, default: '1'},
-    grades: [{ type: String, ref: 'Grades', index: true }], // Obj.Id
-    universities: [{type: Schema.ObjectId, required: false, ref: 'University'}],
+	studentId: { type: String, required: true },
+	modelNum: { type: String, required: true },
+	subjects: [{ type: String, ref: 'Subject', index: true  }], // Obj.Id
+	completedSubjects: [{ type: Schema.ObjectId, ref: 'Subject', index: true }], // Obj.Id
+	department: { type: Schema.ObjectId, ref: 'Department', required: true }, // Obj.Id
+	degree: { type: String, enum: ['OAS', 'MAS', 'DAS'] },
+	status: { type: String, enum: ['Budžet', 'Samofinansiranje', 'Done'], required: true },
+	dateOfEnrollment: { type: String, required: true },
+	currentSemester: { type: String, default: '1'},
+	grades: [{ type: String, ref: 'Grades', index: true }] // Obj.Id
 });
 
 const Student = model<StudentDocument>('Student', StudentSchema);
