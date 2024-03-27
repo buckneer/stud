329afea2306c78e50989a70f2dcf32dea45719ae
Miftari Simel <miftarisimel@gmail.com>
2024-03-27 15:14:44 +0100

    UserItem connected to backend

---
+++ client/src/components/UserItem/UserItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {User} from "../../app/api/types/types";
+import RandomBlob from "../RandomBlob/RandomBlob";
+import { useNavigate } from "react-router-dom";
+
+
+interface UserItemProps {
+	user: User,
+	university: string,
+	role: string
+}
+
+function UserItem({user, university, role}: UserItemProps) {
+
+	const navigate = useNavigate();
+	const handleRedirect = () => {
+		navigate(`/uni/${university}/${role}/add`, )
+	}
+
+	return (
+		<div className="card overflow-hidden p-0  w-full cursor-pointer" onClick={() => handleRedirect()}>
+			<div className="flex justify-between p-5">
+				<div className="flex flex-col justify-center items-center font-black">
+					<div className="">
+						Nije
+					</div>
+					<div className="">
+						Registrovan
+					</div>
+				</div>
+				<div className="icon text-blue-300">
+					<RandomBlob width={100} height={100} />
+				</div>
+			</div>
+			<div className="bg-slate-100 p-3">
+				<h1 className=' font-black'>Neki Korisnik</h1>
+				<p>miftarisimel@gmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default UserItem
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,133 +1,42 @@
 import React from 'react'
 import {GraduationCap} from "lucide-react";
 import RandomBlob from "../../components/RandomBlob/RandomBlob";
+import {useGetPendingQuery} from "../../app/api/userApiSlice";
+import Loader from "../../components/Loader/Loader";
+import UserItem from "../../components/UserItem/UserItem";
+import {Helmet} from "react-helmet";
 
+const university: string = "65fafc2da919db458f7ed90d";
+const role: string = "student";
 export default function Home() {
+
+  const {
+    data,
+    isLoading,
+    isSuccess,
+    isError
+  } = useGetPendingQuery({university, role})
+
   return (
     <div className="bg-white">
+      <Helmet>
+        <title>Početna | Stud</title>
+      </Helmet>
       <div className="flex h-full">
-        <div className="lists-container flex-1">
+        <div className="lists-container flex-1 h-full overflow-y-scroll">
           <div className="list-header flex justify-between p-5 ">
             <h1 className="font-black text-3xl">Studenti</h1>
             <div className="search-container">
               <input className='border-0 rounded-2xl bg-slate-100' type="text" placeholder="Pretraga" />
             </div>
           </div>
-          <div className="lists grid grid-cols-3 gap-10 mx-5">
-            <div className="card overflow-hidden p-0  w-full">
-              <div className="flex justify-between p-5">
-                <div className="flex flex-col justify-center items-center font-black">
-                  <div className="">
-                    1
-                  </div>
-                  <div className="">
-                    Sem
-                  </div>
-                </div>
-                <div className="icon text-blue-300">
-                  <RandomBlob width={100} height={100} />
-                </div>
-              </div>
-              <div className="bg-slate-100 p-3">
-                <h1 className=' font-black'>Neki Korisnik</h1>
-                <p>miftarisimel@gmail.com</p>
-              </div>
-            </div>
-            <div className="card overflow-hidden p-0  w-full">
-              <div className="flex justify-between p-5">
-                <div className="flex flex-col justify-center items-center font-black">
-                  <div className="">
-                    1
-                  </div>
-                  <div className="">
-                    Sem
-                  </div>
-                </div>
-                <div className="icon text-blue-300">
-                  <RandomBlob width={100} height={100} />
-                </div>
-              </div>
-              <div className="bg-slate-100 p-3">
-                <h1 className=' font-black'>Neki Korisnik</h1>
-                <p>miftarisimel@gmail.com</p>
-              </div>
-            </div>
-            <div className="card overflow-hidden p-0  w-full">
-              <div className="flex justify-between p-5">
-                <div className="flex flex-col justify-center items-center font-black">
-                  <div className="">
-                    1
-                  </div>
-                  <div className="">
-                    Sem
-                  </div>
-                </div>
-                <div className="icon text-blue-300">
-                  <RandomBlob width={100} height={100} />
-                </div>
-              </div>
-              <div className="bg-slate-100 p-3">
-                <h1 className=' font-black'>Neki Korisnik</h1>
-                <p>miftarisimel@gmail.com</p>
-              </div>
-            </div>
-            <div className="card overflow-hidden p-0  w-full">
-              <div className="flex justify-between p-5">
-                <div className="flex flex-col justify-center items-center font-black">
-                  <div className="">
-                    1
-                  </div>
-                  <div className="">
-                    Sem
-                  </div>
-                </div>
-                <div className="icon text-blue-300">
-                  <RandomBlob width={100} height={100} />
-                </div>
-              </div>
-              <div className="bg-slate-100 p-3">
-                <h1 className=' font-black'>Neki Korisnik</h1>
-                <p>miftarisimel@gmail.com</p>
-              </div>
-            </div>
-            <div className="card overflow-hidden p-0  w-full">
-              <div className="flex justify-between p-5">
-                <div className="flex flex-col justify-center items-center font-black">
-                  <div className="">
-                    1
-                  </div>
-                  <div className="">
-                    Sem
-                  </div>
-                </div>
-                <div className="icon text-blue-300">
-                  <RandomBlob width={100} height={100} />
-                </div>
-              </div>
-              <div className="bg-slate-100 p-3">
-                <h1 className=' font-black'>Neki Korisnik</h1>
-                <p>miftarisimel@gmail.com</p>
-              </div>
-            </div>
-            <div className="card overflow-hidden p-0  w-full">
-              <div className="flex justify-between p-5">
-                <div className="flex flex-col justify-center items-center font-black">
-                  <div className="">
-                    1
-                  </div>
-                  <div className="">
-                    Sem
-                  </div>
-                </div>
-                <div className="icon text-blue-300">
-                  <RandomBlob width={100} height={100} />
-                </div>
-              </div>
-              <div className="bg-slate-100 p-3">
-                <h1 className=' font-black'>Neki Korisnik</h1>
-                <p>miftarisimel@gmail.com</p>
-              </div>
-            </div>
+          <div className="lists grid grid-cols-3 gap-10 mx-5 overflow-y-auto">
+            {isLoading && <Loader />}
+            {isSuccess && (
+                data.map(user => (
+                    <UserItem user={user} university={university} role={role}/>
+                ))
+            )}
           </div>
         </div>
         <div className='sidebar bg-slate-100 p-5'>
