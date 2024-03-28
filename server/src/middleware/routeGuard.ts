8ec4001594833718c5ebc12cb0ff9886b6247f30
Miftari Simel <miftarisimel@gmail.com>
2024-03-29 00:07:15 +0100

    Route Guard can now check for every role

--- server/src/middleware/routeGuard.ts
+++ server/src/middleware/routeGuard.ts
@@ -5,6 +5,7 @@ import { newError } from "../utils";
 import 'dotenv/config';
 import { UserRequest, UserToken } from './UserRequest';
 import { getService, getServiceByUserId } from '../services/service.service';
+import log from "../logger";
 
 
 
@@ -22,6 +23,7 @@ export const userGuard = async (req: Request, res: Response, next: NextFunction)
             req.user = user as UserToken;
 
             next();
+
         })
     } catch (e: any) {
         console.error(e);
@@ -30,37 +32,42 @@ export const userGuard = async (req: Request, res: Response, next: NextFunction)
 }
 
 
-export const roleGuard = (role: string) => {
+export const roleGuard = (roles: string[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         // Test url: http://localhost:3000/uni/65fafc2da919db458f7ed90d/student/add
         try {
-            console.log(req.user);
-            if(!req.user) throw newError(401, 'Niste ulogovani');
-            if(!req.user.roles.includes(role)) throw newError(403, 'STOJ!');
+
+            if(!req.user) return res.status(401).send(newError(401, 'Niste ulogovani'));
+            if(!req.user.roles.some(item => roles.includes(item))) res.status(403).send(newError(403, 'STOJ!'));
+
+            if(req.user.roles.includes('professor')) {
+                console.log("You are professor");
+                return next();
+
+            }
+
+
+            if(req.user.roles.includes('student')) {
+                console.log("You are student");
+                return next();
+
+            }
 
             if(req.user.roles.includes('service')) {
+                console.log("You are a service");
                 // checks only university
                 // Every service can access every department if on same uni.
-                if(!req.params.university) throw newError(401, 'Id univerziteta je obavezan');
+                if(!req.params.university) return  res.status(401).send(newError(401, 'Id univerziteta je obavezan'));
 
                 let service = await getServiceByUserId(req.user.id);
 
                 let uni = service.university?.toString();
 
 
-                if(uni !== req.params.university) { throw newError(403, 'Ne možete da pristupite ovom univerzitetu') }
+                if(uni !== req.params.university) { return res.status(403).send(newError(403, 'Ne možete da pristupite ovom univerzitetu')) }
                 console.log("YOU PASSED ROUTE GUARD");
-                next();
+                return next();
             }
-            // if (req.user.role == 'professor') {
-            //     // TODO check what professor has access to
-            // }
-
-            // if (req.user.role = 'student') {
-            //     // TODO same for the student
-            // }
-
-            // next();
         } catch (e: any) {
             console.error(e);
             return res.status(e.status || 500).send(e || 'Internal Server Error');
